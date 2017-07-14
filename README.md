769ee5c0d993d82c65c7076a525213e59c6cfc09
Mark Vainomaa <mikroskeem@mikroskeem.eu>
2017-07-14 19:24:45 +0300

    Make example better

--- README.md
+++ README.md
@@ -1,11 +1,67 @@
 # Orion example Mod
 
-This is example mod overriding EULA check on 1.12 Paper server using Mixin
-
-API is not done yet, hence library jar sits in repository as well
+## Description
+This is example mod demonstrating configuration API and overriding EULA check on 1.12 Paper server using Mixin
 
 Put this into mods dir, remove eula.txt and see how it works :smile:
 
-(See Releases for precompiled mod)
+## Requirements
+- Paper 1.12
 
-WTFPL
+## Configuration
+See `modconfigs/examplemod.cfg`, it has one option named `restore-the-check`. Setting that to `true` makes mod not apply
+mixin to the server
+
+## I want to create mod like this!
+Sure, here's appropriate Maven/Gradle configuration
+
+### Maven
+```xml
+<repositories>
+    <repository>
+        <id>mikroskeem-repo</id>
+        <url>https://repo.wut.ee/repository/mikroskeem-repo/</url>
+    </repository>
+</repositories>
+
+<dependencies>
+    <!-- Note: `./paper j` and `mvn install` in Paper-Server directory -->
+    <dependency>
+        <groupId>com.destroystokyo.paper</groupId>
+        <artifactId>paper</artifactId>
+        <version>1.12-R0.1-SNAPSHOT</version>
+        <scope>provided</scope>
+    </dependency>
+
+    <!-- Orion API -->
+    <dependency>
+        <groupId>eu.mikroskeem</groupId>
+        <artifactId>orion.api</artifactId>
+        <version>0.0.1</version>
+        <scope>provided</scope>
+    </dependency>
+</dependencies>
+```
+
+
+### Gradle
+```groovy
+repositories {
+    maven {
+        name = 'mikroskeem-repo'
+        url = 'https://repo.wut.ee/repository/mikroskeem-repo'
+    }
+}
+
+dependencies {
+    compileOnly group: 'com.destroystokyo.paper', name: 'paper', version: '1.12-R0.1-SNAPSHOT'
+    compileOnly group: 'eu.mikroskeem', name: 'orion.api', version: '0.0.1'
+}
+```
+
+
+## I want this mod!
+See [releases](https://github.com/OrionMinecraft/ExampleMod/releases)
+
+## I want to steal your mod's code!
+Go ahead, this mod is [WTFPL](http://www.wtfpl.net/about/)
--- src/main/java/eu/mikroskeem/orion/examplemod/ExampleMod.java
+++ src/main/java/eu/mikroskeem/orion/examplemod/ExampleMod.java
@@ -1,26 +1,58 @@
 package eu.mikroskeem.orion.examplemod;
 
 import com.google.common.eventbus.Subscribe;
-import eu.mikroskeem.orion.api.OrionAPI;
+import eu.mikroskeem.orion.api.Orion;
 import eu.mikroskeem.orion.api.annotations.OrionMod;
 import eu.mikroskeem.orion.api.events.ModConstructEvent;
+import eu.mikroskeem.orion.api.events.ModLoadEvent;
+import eu.mikroskeem.shuriken.common.streams.ByteArrays;
+import ninja.leaping.configurate.commented.CommentedConfigurationNode;
+import ninja.leaping.configurate.loader.ConfigurationLoader;
 import org.apache.logging.log4j.Logger;
 
 import javax.inject.Inject;
+import javax.inject.Named;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
 
 
 /**
- * Orion sample mod
+ * Orion example mod
  *
  * @author Mark Vainomaa
  */
-@OrionMod(id = "samplemod")
+@OrionMod(id = "examplemod")
 public final class ExampleMod {
     @Inject private Logger logger;
+    @Inject private Orion orion;
+    @Inject @Named("configurationPath") private Path configurationPath;
+    @Inject private ConfigurationLoader<CommentedConfigurationNode> configurationLoader;
+    private boolean restoreCheck = false;
 
     @Subscribe
     public void on(ModConstructEvent e) throws Exception {
         logger.info("Hello world!");
-        OrionAPI.getInstance().registerMixinConfig("mixins.examplemod.json");
+        if(Files.notExists(configurationPath)) {
+            Files.write(
+                    configurationPath,
+                    ByteArrays.fromInputStream(
+                            ExampleMod.class.getResourceAsStream("/assets/examplemod/examplemod.cfg")),
+                    StandardOpenOption.CREATE_NEW,
+                    StandardOpenOption.WRITE
+            );
+        }
+        CommentedConfigurationNode baseNode = configurationLoader.load();
+        restoreCheck = baseNode.getNode("restore-the-check").getBoolean(false);
+        configurationLoader.save(baseNode);
+    }
+
+    @Subscribe
+    public void on(ModLoadEvent e) throws Exception {
+        if(restoreCheck) {
+            logger.info("Not applying EULA remover mixin, as user requested not to do so.");
+            return;
+        }
+        orion.registerMixinConfig("mixins.examplemod.json");
     }
 }
